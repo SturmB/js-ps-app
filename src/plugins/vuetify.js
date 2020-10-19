f6fe029a5d5411c71447a5116fa42f4755264310
Chris McGee <chris@chrismcgee.info>
2020-10-19 15:40:07 -0400

    Grid and new breakpoints

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,22 +1,30 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  breakpoint: {
+    thresholds: {
+      xs: 340,
+      sm: 540,
+      md: 800,
+      lg: 1280,
+    },
+  },
   theme: {
-      options: {
-        customProperties: true,
-      },
+    options: {
+      customProperties: true,
+    },
     themes: {
       light: {
-        primary: '#ee44aa',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
+        primary: "#ee44aa",
+        secondary: "#424242",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
       },
     },
   },
