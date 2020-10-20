eafe25f2dc528af9943a28b9d9440fc779c9326e
Chris McGee <chris@chrismcgee.info>
2020-10-20 12:51:39 -0400

    Process View Files

---
+++ src/components/InputFiles.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-row dense>
+    <v-col>
+      <v-file-input
+        v-model="selectedFiles"
+        multiple
+        counter
+        show-size
+        accept="image/*"
+        label="Files"
+        hint="Select input Files"
+        @change="$emit('input-change', selectedFiles)"
+      >
+        <template v-slot:selection="{ index, text }">
+          <v-chip
+            small
+            label
+            color="primary"
+            close
+            @click:close="removeFile(index)"
+            v-if="index < 4"
+            >{{ text }}</v-chip
+          >
+          <span v-else-if="index === 4" class="caption ml-2 grey--text"
+            >+ {{ selectedFiles.length - 4 }} file(s)</span
+          >
+        </template>
+      </v-file-input>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedFiles: [],
+    };
+  },
+  methods: {
+    removeFile(idx) {
+      this.selectedFiles.splice(idx, 1);
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Process.vue
+++ src/views/Process.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    <h1>Process</h1>
-  </div>
+  <v-form>
+    <v-container>
+      <input-files @input-change="updateInputFiles"></input-files>
+    </v-container>
+  </v-form>
 </template>
+
+<script>
+import InputFiles from "@/components/InputFiles";
+export default {
+  components: {
+    InputFiles,
+  },
+  data() {
+    return {
+      files: [],
+    };
+  },
+  methods: {
+    updateInputFiles(filesArray) {
+      this.files = filesArray;
+    },
+  },
+};
+</script>
+
+<style></style>
